27bccb5de18df407cc3d3d67b554d676a3f2ed00
dudz <dzvcs@proton.me>
2025-05-21 17:42:06 -0300

    style: refine SignUp page layout and enhance visual hierarchy

--- src/components/layout/SignUp.tsx
+++ src/components/layout/SignUp.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const SignUpLay = ({ children }: { children: React.ReactNode }) => {
 	return (
-		<div className="bg-white w-2xl rounded-2xl flex py-40 items-center">
-			<div className=" py-12 px-32">
+		<div className="bg-white w-2xl rounded-2xl flex py-24 items-center">
+			<div className="px-24">
 				<form className="flex flex-col gap-6">{children}</form>
 			</div>
 		</div>
--- src/components/page/SignUp.tsx
+++ src/components/page/SignUp.tsx
@@ -12,20 +12,31 @@ const SignUpPage = () => {
 	}
 
 	return (
-		<div className="flex flex-row-reverse items-center h-screen w-full p-16">
-			<SignUp>
-				<InputGroup inputLabel={'Username'} type={'text'} handleChange={onChange} />
-				<InputGroup inputLabel={'Email'} type={'email'} handleChange={onChange} />
-				<InputGroup inputLabel={'Password'} type={'password'} handleChange={onChange} />
-				<InputGroup inputLabel={'terms'} type={'checkbox'} handleChange={onChange} />
-				<Button
-					btnType={'submit'}
-					label={'Sign Up'}
-					handleClick={handleClick}
-					variant={'login'}
-				/>
-			</SignUp>
-			<div>another</div>
+		<div className="flex items-center justify-center w-full h-screen">
+			<div className="flex flex-row-reverse w-full p-16 justify-evenly items-baseline">
+				<SignUp>
+					<h3>SignUp</h3>
+					<InputGroup inputLabel={'Username'} type={'text'} handleChange={onChange} />
+					<InputGroup inputLabel={'Email'} type={'email'} handleChange={onChange} />
+					<InputGroup inputLabel={'Password'} type={'password'} handleChange={onChange} />
+					<InputGroup inputLabel={'terms'} type={'checkbox'} handleChange={onChange} />
+					<Button
+						btnType={'submit'}
+						label={'Sign Up'}
+						handleClick={handleClick}
+						variant={'login'}
+					/>
+				</SignUp>
+				<div className="flex flex-col items-start h-[900px] justify-between">
+					<div>
+						<h1>Design with us</h1>
+						<p className="text-3xl max-w-xl h-max text-white opacity-80">
+							Access to thousands of design resources and templates
+						</p>
+					</div>
+					<img className="" src="/images/Illustration.png" alt="illustration" />
+				</div>
+			</div>
 		</div>
 	);
 };
